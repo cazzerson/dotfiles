acc3d7f93144acbdc1401b225c55e982ae3f19ff
Jason Casden <casden@gmail.com>
2017-02-25 20:37:31 -0500

    Streamline submodule updates

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,11 +1,11 @@
-git submodule update --init --recursive
-git submodule foreach 'git checkout master && git pull origin master'
+git submodule update --init --recursive --remote
+#git submodule foreach 'git checkout master && git pull origin master'
 
 #
 # zim_update - update the zim repository
 #
 
-cd ${ZDOTDIR:-${HOME}/.zim}
+cd tag-zsh/zim
 
 # this is the cleanest way I know of to update a repository
 git remote update -p
