b1f5867116257a59900075c28e516467fa439294
Jason Casden <casden@gmail.com>
2019-02-07 11:16:46 -0500

    Simplify submodule updates

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,16 +1,2 @@
 git submodule update --init --recursive --remote
-#git submodule foreach 'git checkout master && git pull origin master'
 
-#
-# zim_update - update the zim repository
-#
-
-pushd tag-zsh/zim
-
-# this is the cleanest way I know of to update a repository
-git remote update -p
-git merge --ff-only @\{u\}
-# and update the submodules
-git submodule foreach --quiet --recursive git pull origin master
-
-popd
